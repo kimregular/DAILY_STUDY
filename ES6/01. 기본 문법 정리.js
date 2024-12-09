93081470551a3f19638bdbf66e0011c07975e8ef
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-09 21:49:08 +0900

    obsidian backup: 2024-12-09 21:49:08

--- ES6/01. 기본 문법 정리.js
+++ ES6/01. 기본 문법 정리.js
@@ -25,3 +25,15 @@ function 작명2(파라미터) {
 document.getElementById("button").addEventListener("click", function () {
     작명2("이제 이걸로 글자가 설정됨")
 });
+
+// 자료를 한번에 여러개 저장하려면 array, object를 사용한다.
+var myName = "kim";
+var names = ["kim", "lee"]; // array
+var myNames = { // object 값 옆에 이름을 붙여줘야함
+    name: 'kim',
+    age: 20,
+};
+
+// array와 object 자료형 접근하기
+console.log(names[0]); // "kim" 출력
+console.log(myNames.name); // "kim" 출력
