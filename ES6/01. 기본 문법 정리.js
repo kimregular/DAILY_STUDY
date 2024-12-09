628b3395760baf22f3a1853019d07ffe09c20ab7
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-09 21:54:08 +0900

    obsidian backup: 2024-12-09 21:54:08

--- ES6/01. 기본 문법 정리.js
+++ ES6/01. 기본 문법 정리.js
@@ -37,3 +37,39 @@ var myNames = { // object 값 옆에 이름을 붙여줘야함
 // array와 object 자료형 접근하기
 console.log(names[0]); // "kim" 출력
 console.log(myNames.name); // "kim" 출력
+console.log(myNames["name"]) // "kim" 출력
+
+// 코드를 조건부로 실행하고 싶을 때
+// 조건식 부분이 true라면 괄호 안의 코드가 동작한다.
+if(조건식) {
+    console.log("안녕");
+}
+
+// 조건식 부분이 거짓일 때 동작하는 코드를 설정하고 싶다면 else 부분에 작성한다.
+if(조건식) {
+    console.log("안녕");
+} else {
+    console.log("안녕2");
+}
+
+// 여러 조건을 동시에 구분하고 싶다면 else if 를 사용한다.
+
+if(조건식) {
+    console.log("안녕");
+} else if (조건식2) {
+    console.log("안녕2");
+} else {
+    console.log("안녕3");
+}
+
+// 코드를 반복하고 싶다면 for문 사용
+// 안녕! 을 5번 반복한다.
+for (var i = 0; i < 5; i++) {
+    console.log("안녕!");
+}
+// forEach
+// array의 길이만큼 function이 동작한다.
+// 안녕!이 4번 출력된다.
+[1,2,3,4].forEach(function() {
+    console.log("안녕!");
+})
