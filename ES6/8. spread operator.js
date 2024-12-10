1ee56430e0bd2eee2b9b98be4194aebf3ef0afd6
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-10 11:38:56 +0900

    obsidian backup: 2024-12-10 11:38:55

--- ES6/8. spread operator.js
+++ ES6/8. spread operator.js
@@ -14,7 +14,7 @@ console.log(문자);
 console.log(...문자);
 
 // 그래서 어디에 씀?
-// 1. array 합치기, 복사
+// 1. array 합치기
 var a = [1, 2, 3];
 var b = [4, 5];
 var c = [a, b];
@@ -22,8 +22,22 @@ var d = [...a, ...b];
 console.log(c); // 2중 배열
 console.log(d); // 배열
 
-// 2. deep copy
+// 2. 배열 deep copy
 var x = [1, 2, 3];
 var y = x;
 var z = [...x];
 // x와 y는 같은 인스턴스를 참조하지만 z는 완전 새로운 인스턴스를 참조한다.
+
+// 3. 오브젝트 합치기
+var o1 = {a: 1, b: 2};
+var o2 = {o1, c: 3};
+var o3 = {...o1, d: 4};
+console.log(o2); // { o1: { a: 1, b: 2 }, c: 3 }
+console.log(o3); // { a: 1, b: 2, d: 4 }
+
+// 4. 오브젝트 딥카피
+var ox = {a: 3};
+var oy = ox;
+oy.b = 4;
+console.log(ox); // { a: 3, b: 4 }
+console.log(oy); // { a: 3, b: 4 }
