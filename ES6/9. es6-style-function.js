43ca4ca42b93d8f77f094d33091011d169dcc6b2
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-10 12:58:30 +0900

    obsidian backup: 2024-12-10 12:58:30

--- ES6/9. es6-style-function.js
+++ ES6/9. es6-style-function.js
@@ -11,3 +11,28 @@ function 더하기(a, b) {
 더하기(1); // NaN
 
 // default 파라미터를 알아보자.
+function 더하기default(a, b = 10) {
+    console.log(a + b);
+}
+
+더하기default(1, 2); // 3
+더하기default(1); // 11
+// b 자리에 아무 값도 없다면 저절로 10을 가지게 된다.
+// b 자리에 값이 있다면 10은 무시된다.
+
+// 기본값으로 함수를 지정할 수도 있다.
+function 임시() {
+    return 10;
+}
+function 더하기defaultFunc(a, b = 임시()) {
+    console.log(a + b);
+}
+
+더하기defaultFunc(1, 2); // 3
+더하기defaultFunc(1); // 11
+
+// 함수의 arguments를 알아보자.
+
+function 더하기2(a, b, c) { // 여기의 a, b, c 는 파라미터
+    console.log(a, b, c); // 여기의 a, b, c 는 arguments
+}
