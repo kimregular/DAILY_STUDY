6b9bdfb9a931e2056a66cdbfe6d0afff1e5c5d12
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-10 17:47:33 +0900

    obsidian backup: 2024-12-10 17:47:33

--- ES6/12. es5 상속.js
+++ ES6/12. es5 상속.js
@@ -9,4 +9,12 @@ var 자식 = Object.create(부모);
 // prototype을 부모로 해주세요 라는 뜻이다.
 
 console.log(자식); // 자식이라는 오브젝트에는 아무것도 없다.
-console.log(자식.name);
+console.log(자식.name); // 자식의 부모가  name을 갖고있기 때문에 정상 출력
+// 자식의 나이가 50인데 바꾸려면?
+자식.age = 20;
+console.log(자식.age); // 이제부터는 자식이 age를 갖고있으므로 부모까지 가서 확인하지 않는다.
+
+var 손자 = Object.create(자식);
+console.log(손자.name);
+console.log(손자.age);
+// 자식의 age 값이 20이므로 20출력
---
+++ ES6/13. es6 상속.js
@@ -0,0 +1,3 @@
+// class 문법을 알아보자.
+// constructor, prototype을 이용한 상속기능을 만들 수 있게 도와주는 문법이다.
+
