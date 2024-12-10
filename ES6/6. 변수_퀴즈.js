8aa3cfc8aae201418ee5f269a1fc6da09241441c
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-10 10:03:18 +0900

    obsidian backup: 2024-12-10 10:03:18

---
+++ ES6/6. 변수_퀴즈.js
@@ -0,0 +1,27 @@
+// 다음 코드의 콘솔창 출력 결과는 뭘까요?
+함수1();
+function 함수1() {
+    console.log(안녕);
+    let 안녕 = "Hello";
+}
+
+// 다음 코드의 콘솔창 출력 결과는 뭘까요?
+함수2();
+function 함수2() {
+    console.log(안녕);
+    var 안녕 = "Hello";
+}
+
+// 다음 코드의 콘솔창 출력 결과는 뭘까요?
+let a = 1;
+var 함수3 = function() {
+    a = 2;
+}
+console.log(a);
+
+// 다음 코드의 콘솔창 출력 결과는 뭘까요?
+let x = 1;
+var y = 2;
+window.x = 3;
+window.y = 4;
+console.log(x + y);
