efbe99e0f156e69da65a1620c944242a9a4fe352
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-11 14:10:08 +0900

    obsidian backup: 2024-12-11 14:10:08

--- ES6/20. async await.js
+++ ES6/20. async await.js
@@ -9,17 +9,31 @@
 // async await 키워드를 사용해보자.
 
 // async
-async function 더하기() {
+async function 더하기1() {
     // async를 함수 앞에 붙이면 함수가 promise의 역할을 할 수 있다.
     // 함수 실행 후에 promise 오브젝트가 남는다
     return 1 + 1;
     // return 문은 then이나 catch의 파라미터로 들어간다
 }
 
-더하기().then(function(결과) {
+더하기1().then(function(결과) {
     console.log(`성공이에요! -> ${결과}`);
     // async 함수의 return 값을 파라미터로 받는다.
 });
 
+// await
+// then 함수를 쓰기 싫다면 await 키워드를 사용할 수 있다.
+// await은 async 키워드를 쓴 함수 안에서만 사용할 수 있다는 특징이 있다.
+// 간단하게 프로미스.then() 대체품으로 생각하자
 
+// 어려운 연산을 하고나서 성공/실패를 판정해주는 promise를 만들고 싶다고 가정해보자.
+async function 더하기2() {
+    var 어려운연산 = new Promise(function (성공, 실패) {
+        var 결과 = 1 + 1;
+        성공(결과);
+    });
+    어려운연산.then(
+        console.log(결과);
+    )
+}
 
