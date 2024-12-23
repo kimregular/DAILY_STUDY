5d70996ea909fcf9031999a34545f5064c76603a
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-23 22:23:32 +0900

    obsidian backup: 2024-12-23 22:23:32

--- VUE/vuestagram/vuestagram/src/App.vue
+++ VUE/vuestagram/vuestagram/src/App.vue
@@ -1,26 +1,108 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+        <ul class="header-button-left">
+            <li>Cancel</li>
+        </ul>
+        <ul class="header-button-right">
+            <li>Next</li>
+        </ul>
+        <img src="./assets/logo.png" class="logo" />
+    </div>
+
+    <Container />
+
+    <div class="footer">
+        <ul class="footer-button-plus">
+            <input type="file" id="file" class="inputfile" />
+            <label for="file" class="input-plus">+</label>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
+body {
+    margin: 0;
+}
+ul {
+    padding: 5px;
+    list-style-type: none;
+}
+.logo {
+    width: 22px;
+    margin: auto;
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 13px;
+}
+.header {
+    width: 100%;
+    height: 40px;
+    background-color: white;
+    padding-bottom: 8px;
+    position: sticky;
+    top: 0;
+}
+.header-button-left {
+    color: skyblue;
+    float: left;
+    width: 50px;
+    padding-left: 20px;
+    cursor: pointer;
+    margin-top: 10px;
+}
+.header-button-right {
+    color: skyblue;
+    float: right;
+    width: 50px;
+    cursor: pointer;
+    margin-top: 10px;
+}
+.footer {
+    width: 100%;
+    position: sticky;
+    bottom: 0;
+    padding-bottom: 10px;
+    background-color: white;
+}
+.footer-button-plus {
+    width: 80px;
+    margin: auto;
+    text-align: center;
+    cursor: pointer;
+    font-size: 24px;
+    padding-top: 12px;
+}
+.sample-box {
+    width: 100%;
+    height: 600px;
+    background-color: bisque;
+}
+.inputfile {
+    display: none;
+}
+.input-plus {
+    cursor: pointer;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    box-sizing: border-box;
+    font-family: "consolas";
+    margin-top: 60px;
+    width: 100%;
+    max-width: 460px;
+    margin: auto;
+    position: relative;
+    border-right: 1px solid #eee;
+    border-left: 1px solid #eee;
 }
 </style>
