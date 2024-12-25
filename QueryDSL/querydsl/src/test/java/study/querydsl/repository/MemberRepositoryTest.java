40dde458a94e9f0eef759d995ea77880153956d1
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-26 08:37:00 +0900

    obsidian backup: 2024-12-26 08:37:00

--- QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import java.util.List;
 import java.util.Optional;
@@ -42,6 +45,35 @@ class MemberRepositoryTest {
 
         assertThat(foundMemberWithName.get(0)
                                       .getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    @DisplayName("search test")
+    void test3(){
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+        // when
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberRepository.search(condition);
+        // then
+        assertThat(result).extracting("username")
+                          .containsExactly("member4");
     }
 
 }
