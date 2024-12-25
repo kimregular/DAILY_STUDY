2ee57089f4ba1e16609ed171e4d90780362fa532
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-26 06:59:00 +0900

    obsidian backup: 2024-12-26 06:59:00

--- QueryDSL/querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ QueryDSL/querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -4,9 +4,12 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 
 import java.util.List;
 import java.util.Optional;
+
+import static study.querydsl.entity.QMember.*;
 
 @Repository
 public class MemberJpaRepository {
@@ -33,9 +36,20 @@ public class MemberJpaRepository {
                  .getResultList();
     }
 
+    public List<Member> findAll_dsl() {
+        return queryFactory.selectFrom(member)
+                           .fetch();
+    }
+
     public List<Member> findByUsername(String username) {
         return em.createQuery("select m from Member m where m.username =: username", Member.class)
                  .setParameter("username", username)
                  .getResultList();
+    }
+
+    public List<Member> findByUsername_dsl(String username) {
+        return queryFactory.selectFrom(member)
+                           .where(member.username.eq(username))
+                           .fetch();
     }
 }
--- QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -11,6 +11,7 @@ import study.querydsl.entity.Member;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -42,5 +43,19 @@ class MemberJpaRepositoryTest {
 
         assertThat(foundMemberWithName.get(0)
                                       .getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    @DisplayName("basicQuerydslTest")
+    void test2(){
+        // given
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+        // when
+        List<Member> members = memberJpaRepository.findAll_dsl();
+        List<Member> foundMember = memberJpaRepository.findByUsername_dsl("member1");
+        // then
+        assertThat(members).containsExactly(member);
+        assertThat(foundMember).containsExactly(member);
     }
 }
