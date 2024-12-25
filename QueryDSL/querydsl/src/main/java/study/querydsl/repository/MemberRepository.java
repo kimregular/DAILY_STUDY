bda7cdbc021454e83166c0edd737a9e8071b2c3f
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-26 08:25:00 +0900

    obsidian backup: 2024-12-26 08:25:00

--- QueryDSL/querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ QueryDSL/querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,4 +1,11 @@
 package study.querydsl.repository;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
 }
--- QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,4 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
+package study.querydsl.repository;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
 class MemberRepositoryTest {
-  
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("basicTest")
+    void test1() {
+        // given
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+        // when
+        Optional<Member> foundMember = memberRepository.findById(member.getId());
+        List<Member> members = memberRepository.findAll();
+        List<Member> foundMemberWithName = memberRepository.findByUsername("member1");
+
+        // then
+        assertThat(foundMember).isPresent()
+                               .contains(member);
+
+        assertThat(members).contains(member);
+
+        assertThat(foundMemberWithName.get(0)
+                                      .getUsername()).isEqualTo("member1");
+    }
+
 }
