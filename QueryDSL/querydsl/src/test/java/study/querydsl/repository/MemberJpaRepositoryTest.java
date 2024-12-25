a0a07802f1af44229764f180bc88733fc0bc8748
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-12-26 06:54:00 +0900

    obsidian backup: 2024-12-26 06:54:00

--- QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -1,4 +1,46 @@
-import static org.junit.jupiter.api.Assertions.*;
+package study.querydsl.repository;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
 class MemberJpaRepositoryTest {
-  
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    @DisplayName("basicTest")
+    void test1() {
+        // given
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+        // when
+        Optional<Member> foundMember = memberJpaRepository.findById(member.getId());
+        List<Member> members = memberJpaRepository.findAll();
+        List<Member> foundMemberWithName = memberJpaRepository.findByUsername("member1");
+
+        // then
+        assertThat(foundMember).isPresent()
+                               .contains(member);
+
+        assertThat(members).contains(member);
+
+        assertThat(foundMemberWithName.get(0)
+                                      .getUsername()).isEqualTo("member1");
+    }
 }
