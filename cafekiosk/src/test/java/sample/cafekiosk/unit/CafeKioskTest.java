a574c4bd383ccff75038db599a26cf025f32eaef
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-01-28 19:25:14 +0900

    obsidian backup: 2025-01-28 19:25:14

--- cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -5,13 +5,25 @@ import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CafeKioskTest {
 
     @Test
-    @DisplayName("add 테스트")
+    @DisplayName("add 테스트 - manualTest")
     void test1(){
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+
+        // when
+        cafeKiosk.add(new Americano());
+
+        // then
+        assertThat(cafeKiosk.getBeverages().get(0).getPrice()).isEqualTo(4000);
+    }
+
+    @Test
+    @DisplayName("add 테스트 - automatedTest")
+    void test2(){
         // given
         CafeKiosk cafeKiosk = new CafeKiosk();
 
--- cafekiosk/src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AmericanoTest {
 
@@ -16,5 +15,15 @@ class AmericanoTest {
         // when
         // then
         assertThat(americano.getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    @DisplayName("가격 확인 테스트")
+    void test2() {
+        // given
+        Americano americano = new Americano();
+        // when
+        // then
+        assertThat(americano.getPrice()).isEqualTo(4000);
     }
 }
