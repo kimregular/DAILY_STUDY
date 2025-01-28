c58bcab71961692d0dce8b4a72b00190a2f2e674
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-01-28 23:45:32 +0900

    obsidian backup: 2025-01-28 23:45:32

--- cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.spring.api.service.order;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRespository;
 import sample.cafekiosk.spring.domain.product.ProductType;
@@ -28,7 +31,20 @@ class OrderServiceTest {
 	private OrderService orderService;
 
 	@Autowired
+	private OrderRepository orderRepository;
+
+	@Autowired
 	private ProductRespository productRespository;
+
+	@Autowired
+	private OrderProductRepository orderProductRepository;
+
+	@AfterEach
+	void tearDown() {
+		orderProductRepository.deleteAllInBatch();
+		productRespository.deleteAllInBatch();
+		orderRepository.deleteAllInBatch();
+	}
 
 	@Test
 	@DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
