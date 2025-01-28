6d0c557b7ae6f80358cbd498b78140083d17c05b
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-01-28 21:59:14 +0900

    obsidian backup: 2025-01-28 21:59:14

--- cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRespositoryTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/spring/domain/product/ProductRespositoryTest.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
-import static sample.cafekiosk.spring.domain.product.ProductType.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ProductRespositoryTest {
 
@@ -26,9 +31,29 @@ class ProductRespositoryTest {
                 .name("아메리카노")
                 .price(4000)
                 .build();
+        Product product2 = Product.builder()
+                .productNumber("002")
+                .type(HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+        Product product3 = Product.builder()
+                .productNumber("003")
+                .type(HANDMADE)
+                .sellingStatus(STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+        productRespository.saveAll(List.of(product1, product2, product3));
         // when
-
+        List<Product> products = productRespository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
         // then
-        assertThat()
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001", "아메리카노", SELLING),
+                        tuple("002", "카페라떼", HOLD)
+                );
     }
 }
